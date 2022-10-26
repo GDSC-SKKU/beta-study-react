1892b2909dfccc277d751a6c08dd7733fa2f6543
장재원 <jaychang99@scg.skku.ac.kr>
2022-10-26 15:37:41 +0900

    feat(frontend): 메인페이지에 FriendListItem 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FriendListItem from "./FriendListItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="iphone-container">
+      <div className="center-container">
+        <h1 className="section-title">친구</h1>
+        <FriendListItem
+          imageSource="images/profile/profile_1.jpg"
+          name="이민수"
+          message="아 집에가고 싶다"
+        />
+        <FriendListItem
+          imageSource="images/profile/profile_2.jpg"
+          name="김현수"
+        />
+        <FriendListItem
+          imageSource="images/profile/profile_3.jpg"
+          name="한솔지"
+          message="한결같은 마음"
+        />
+        <FriendListItem
+          imageSource="images/profile/profile_4.jpg"
+          name="김다은"
+        />
+      </div>
     </div>
   );
 }
