b78dde1756d7ee0d7f863d3c4825849599988922
장재원 <jaychang99@scg.skku.ac.kr>
2022-10-26 15:47:55 +0900

    practice(frontend): 빈칸뚫기

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import "./App.css";
-import FriendListItem from "./FriendListItem";
+// 여기에 내가 만든 FriendListItem 을 불러와보세요!
 
 function App() {
   return (
     <div className="iphone-container">
       <div className="center-container">
         <h1 className="section-title">친구</h1>
-        <FriendListItem
-          imageSource="images/profile/profile_1.jpg"
-          name="이민수"
-          message="아 집에가고 싶다"
-        />
-        <FriendListItem
-          imageSource="images/profile/profile_2.jpg"
-          name="김현수"
-        />
-        <FriendListItem
-          imageSource="images/profile/profile_3.jpg"
-          name="한솔지"
-          message="한결같은 마음"
-        />
-        <FriendListItem
-          imageSource="images/profile/profile_4.jpg"
-          name="김다은"
-        />
+        {/* 여기서부터 코드를 수정해보세요! */}
+
+        {/* 여기까지 코드를 수정해보세요! */}
       </div>
     </div>
   );
--- src/FriendListItem.js
+++ src/FriendListItem.js
@@ -3,16 +3,15 @@ import "./friendListItem.css";
 function FriendListItem(props) {
   return (
     <div className="profile-item">
-      <img
+      {/* 이미지를 넣어보세요, 이미지의 스타일된 class 는 profile-image 입니다! */}
+      {/* 이미지의 가로 세로 크기는 모두 40px 로 하면 가장 이뻐요! */}
+      {/* <img...
         className="profile-image"
-        src={props.imageSource}
-        alt="profile"
-        height="40px"
-        width="40px"
-      />
+        ...
+      /> */}
       <div className="profile-text">
-        <p className="profile-name">{props.name}</p>
-        <p className="profile-message">{props.message}</p>
+        <p className="profile-name">{/* 여기를 수정 */}</p>
+        <p className="profile-message">{/* 여기를 수정 */}</p>
       </div>
     </div>
   );
