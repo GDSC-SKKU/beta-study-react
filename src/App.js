f7704af9139e2f61a79fd2259881c83c5bfd31e1
장재원 <jaychang99@scg.skku.ac.kr>
2022-10-31 21:19:20 +0900

    feat(frontend): FRIENDS_LIST mockup 데이터에서 id 값을 갖도록 수정

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import FriendListItem from "./FriendListItem";
 
 const FRIENDS_LIST = [
   {
+    id: 1,
     imageSource: "images/profile/profile_1.jpg",
     name: "이민수",
     message: "아 집에가고 싶다",
   },
   {
+    id: 2,
     imageSource: "images/profile/profile_2.jpg",
     name: "김현수",
   },
   {
+    id: 3,
     imageSource: "images/profile/profile_3.jpg",
     name: "한솔지",
     message: "한결같은 마음",
   },
   {
+    id: 4,
     imageSource: "images/profile/profile_4.jpg",
     name: "김다은",
   },
@@ -27,9 +31,9 @@ function App() {
     <div className="iphone-container">
       <div className="center-container">
         <h1 className="section-title">친구</h1>
-        {FRIENDS_LIST.map((friend, index) => (
+        {FRIENDS_LIST.map((friend) => (
           <FriendListItem
-            key={index}
+            key={friend.id}
             imageSource={friend.imageSource}
             name={friend.name}
             message={friend.name}
