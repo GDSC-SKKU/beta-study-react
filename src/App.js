5637d176510c7808ee79a6700a08c631997697a7
장재원 <jaychang99@scg.skku.ac.kr>
2022-10-31 11:59:21 +0900

    refactor(frontend): 친구 목록을 하드 코딩에서 .map 사용하는 방식으로 변경

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import "./App.css";
 import FriendListItem from "./FriendListItem";
 
+const FRIENDS_LIST = [
+  {
+    imageSource: "images/profile/profile_1.jpg",
+    name: "이민수",
+    message: "아 집에가고 싶다",
+  },
+  {
+    imageSource: "images/profile/profile_2.jpg",
+    name: "김현수",
+  },
+  {
+    imageSource: "images/profile/profile_3.jpg",
+    name: "한솔지",
+    message: "한결같은 마음",
+  },
+  {
+    imageSource: "images/profile/profile_4.jpg",
+    name: "김다은",
+  },
+];
+
 function App() {
   return (
     <div className="iphone-container">
       <div className="center-container">
         <h1 className="section-title">친구</h1>
-        <FriendListItem
-          imageSource="images/profile/profile_1.jpg"
-          name="이민수"
-          message="아 집에가고 싶다"
-        />
-        <FriendListItem
-          imageSource="images/profile/profile_2.jpg"
-          name="김현수"
-        />
-        <FriendListItem
-          imageSource="images/profile/profile_3.jpg"
-          name="한솔지"
-          message="한결같은 마음"
-        />
-        <FriendListItem
-          imageSource="images/profile/profile_4.jpg"
-          name="김다은"
-        />
+        {FRIENDS_LIST.map((friend) => (
+          <FriendListItem
+            imageSource={friend.imageSource}
+            name={friend.name}
+            message={friend.name}
+          />
+        ))}
       </div>
     </div>
   );
